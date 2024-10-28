a66a67d5f6c9eb43234da6e74eec9377a7a88b18
lukboy223 <ltvanekeren@gmail.com>
2024-10-28 16:45:12 +0100

    magazijn error fixing

--- MVC-Framework/app/controllers/Magazijns.php
+++ MVC-Framework/app/controllers/Magazijns.php
@@ -20,7 +20,25 @@ class Magazijns extends BaseController
         ];
 
         $data['Magazijn'] = $this->magaijnModel->GetMagaijnProduct();
+        if (is_null($data['Magazijn'])) {
+            $this->indexError();
+        }
 
         $this->view('magazijns/index', $data);
     }
+
+    public function indexError($errorMessage = "Er is een probleem opgetreden met de database probeer het opnieuw op een later moment.")
+    {
+        $data = [
+            'Magazijn'             => null,
+            'message'               => $errorMessage,
+            'messageColor'          => 'alert-danger',
+            'messageVisibility'     => 'flex'
+        ];
+
+        $data['Magazijn'] = $this->magaijnModel->GetMagaijnProduct();
+
+        $this->view('magazijns/index', $data);
+    }
+
 }
