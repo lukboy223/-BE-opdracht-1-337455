a092f7aba38214efc2c23cf506fbfc692f5d7af2
lukboy223 <ltvanekeren@gmail.com>
2024-10-28 16:57:23 +0100

    commit 6

--- MVC-Framework/app/controllers/Magazijns.php
+++ MVC-Framework/app/controllers/Magazijns.php
@@ -75,7 +75,7 @@ class Magazijns extends BaseController
                 'messageVisibility'     => 'none'
             ];
             $this->view('Magazijns/viewLeverancier', $data);
-            //5
+            //6
         }
     }
 
